fefe6cade5434d5003fd2a93fa3e71a70873ba12
Matheus Gabriel <102601656+theusgab@users.noreply.github.com>
2022-08-10 19:37:10 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+// Faça concatenção dos numeros digitados pelo usuario.
+
+let num1 = prompt ('Digite um número');
+let num2 = prompt ('Digite outro número');
+
+num1= Number (num1);
+num2= Number (num2);
+
+const result = num1 + num2;
+alert ('O resultado da sua conta foi: ' +result);
+
+
